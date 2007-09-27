f20f8ef2691498883ea2f3c8ef552d8f9fa8d10e
Simon Arlott <simon.arlott.org.uk>
2007-09-27 18:23:41 +0000

    set groups to gid close extra socket fd

--- writesocket.c
+++ writesocket.c
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE
+#include <grp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -15,8 +16,13 @@ int main(int argc, char *argv[]) {
 
 	if (argc > 3) {
 		gid_t gid = atoi(argv[3]);
+		gid_t groups[1] = { gid };
+
 		if (setresgid(gid, gid, gid)!=0)
 			{ perror("setresgid"); return 1; }
+
+		if (setgroups(1, groups)!=0)
+			{ perror("setgroups"); return 1; }
 	}
 
 	if (argc > 2) {
@@ -40,6 +46,9 @@ int main(int argc, char *argv[]) {
 
 	if (dup2(s, 1)!=1)
 		{ perror("dup2"); return 1; }
+
+	if (close(s)!=0)
+		{ perror("close"); return 1; }
 
 	execlp("cat", "writesocket", NULL);
 	perror("execlp");
